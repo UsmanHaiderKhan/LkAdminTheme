405b75f581538d29edee2d5eb2e175b953aca906
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2020-11-11 01:27:56 +0500

    Update added drop down through dropdown component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DropDownModel } from '../dropdown/dropdown.component';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  listOfDropDown: DropDownModel[];
+  constructor() {
+    this.listOfDropDown = new Array<DropDownModel>();
+    this.listOfDropDown.push({
+      text: "dropdownList",
+      link: "DashBoard",
+    });
+  }
 
   ngOnInit(): void {
     this.openNav();
   }
   openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+  }
+  dropdownShow() {
+    var element = document.querySelector("dropdown");
+    element.classList.toggle("show");
   }
 }
