41205e76cd5127bfd02b1c71b503c4f1ab13c7d9
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2020-11-25 00:14:29 +0500

    Update : import the chart js modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DropdownComponent, DropDownModel } from './dropdown/dropdown.component';
+import { TableComponent } from './table/table.component';
+import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
 // import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -16,12 +24,20 @@ import { DropdownComponent, DropDownModel } from './dropdown/dropdown.component'
     SidebarComponent,
     DashboardComponent,
     DropdownComponent,
+    TableComponent,
+    LineChartComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PieChartComponent,
+    BubbleChartComponent,
     // DropDownModel
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
